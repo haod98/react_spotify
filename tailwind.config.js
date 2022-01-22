55445a34c4e8f5edb76eab073c14ea929bb094ad
Hao <74541600+haod98@users.noreply.github.com>
2022-01-22 15:41:46 +0100

    Add Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx}"
   ],
   theme: {
+    fontFamily: {
+      'sans': ["Titillium Web", "sans-serif"],
+      'heading': ["Anton", "sans-serif"]
+    },
+    colors: {
+      'primary': '#fa8072',
+      'secondary': '#0f52ba'
+    },
     extend: {},
   },
   plugins: [],
