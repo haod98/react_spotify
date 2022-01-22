47ff90b11edd1dd85236156b7deca8e23fbeef6d
Hao <74541600+haod98@users.noreply.github.com>
2022-01-22 15:16:02 +0100

    Add Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Hero from "./component/landingPage/Hero";
 import { userHasAccessToken, getAccessToken } from "./helpers/authentication";
 import Tracks from "./component/tracks/Tracks";
+import './index.css';
 import SpotifyWebApi from "spotify-web-api-js";
 function App() {
   if (userHasAccessToken()) {
