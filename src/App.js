25f37fe78b332ed86db759951d369b66aacfe8d3
Hao <74541600+haod98@users.noreply.github.com>
2022-01-23 11:43:46 +0100

    Add response styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { userHasAccessToken, getAccessToken } from "./helpers/authentication";
 import Tracks from "./component/tracks/Tracks";
 import SpotifyWebApi from "spotify-web-api-js";
 function App() {
-  document.querySelector('body').classList.add('bg-secondary');
+  document.querySelector('body').classList.add('bg-secondary', 'text-white');
   if (userHasAccessToken()) {
     const spotify = new SpotifyWebApi();
     spotify.setAccessToken(getAccessToken());
--- src/component/tracks/Tracks.js
+++ src/component/tracks/Tracks.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-
 function Tracks(props) {
     const [limit, setLimit] = useState(10);
     const [response, setResponse] = useState(null);
@@ -25,26 +24,28 @@ function Tracks(props) {
                 const songUri = track.uri;
                 let artistNames = '';
                 return (
-                    <div key={i} className="albumContainer">
-                        <a href={songUrl}>
-                            <img src={albumImage} alt="" />
-                        </a>
-                        {
-                            artistsData.map((artist, index) => {
-                                if (artistsData.length - 1 === index) { //If last artist of collab song remove ","
-                                    artistNames += `${artist.name}`
-                                } else if (artistsData.length > 0) { //If not last artist add ","
-                                    artistNames += `${artist.name}, `;
-                                } else {
-                                    artistNames += `${artist.name}`; //If single artist add nothing
-                                };
-                            })
-                        }
-                        <div className="trackInfoContainer">
-                            <p className="text-3xl font-heading">{track.name}</p>
-                            <p className="trackArtist">{artistNames}</p>
+                    <div key={i} className="flex flex-col items-center my-12 md:my-1">
+                        <div className="outline outline-primary outline-3 md:flex md:items-center md:gap-5">
+                            <a href={songUrl}>
+                                <img src={albumImage} alt="" className="md:max-w-[160px]" />
+                            </a>
+                            {
+                                artistsData.map((artist, index) => {
+                                    if (artistsData.length - 1 === index) { //If last artist of collab song remove ","
+                                        artistNames += `${artist.name}`
+                                    } else if (artistsData.length > 0) { //If not last artist add ","
+                                        artistNames += `${artist.name}, `;
+                                    } else {
+                                        artistNames += `${artist.name}`; //If single artist add nothing
+                                    };
+                                })
+                            }
+                            <div className="my-2 max-w-[300px] min-w-[300px] py-6">
+                                <p className="text-2xl font-heading text-center">{track.name}</p>
+                                <p className="text-primary text-center font-semibold">{artistNames}</p>
+                            </div>
+                            <iframe className="songIframe w-[300px] h-[80px]" title={track.name} src={"https://open.spotify.com/embed/track/" + getSongUri(songUri)}></iframe>
                         </div>
-                        <iframe className="songIframe" title={track.name} src={"https://open.spotify.com/embed/track/" + getSongUri(songUri)} width="300" height="80" allowtransparency="true"></iframe>
                     </div>
                 )
             })
