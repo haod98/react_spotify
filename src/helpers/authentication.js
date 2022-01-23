26c0bfb210ef0b38c1a467b8952305d614207f23
Hao <74541600+haod98@users.noreply.github.com>
2022-01-23 12:03:58 +0100

    Add github link for github pages

--- src/helpers/authentication.js
+++ src/helpers/authentication.js
@@ -1,6 +1,6 @@
 export function authentication() {
     const client_id = 'd9a8b2f7634d48549a496954be94156f';
-    const redirect_uri = 'http://localhost:3000/'
+    const redirect_uri = 'https://haod98.github.io/spotify_api/'
     const scope = encodeURI('user-top-read playlist-modify-public');
     return `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=${scope}`
 };
